53b9d9b895890fd081c769aa8a5f0cd02d771716
Ronildo22 <ronildo.santos224@gmail.com>
2025-03-31 07:38:57 -0300

    second commit

--- test/test_caesar_cipher.py
+++ test/test_caesar_cipher.py
@@ -13,7 +13,7 @@ def test_decript_caesar_cipher_with_word_exists_in_alphabet():
     from src.caesar_cipher import CaesarCipher
 
     caesar_cipher = CaesarCipher(shift_to_right=4)
-    word_encript = "ronildo o melhor de todos"
+    word_encript = caesar_cipher.encript("ronildo o melhor de todos")
 
     word_decript = caesar_cipher.decript(text=word_encript)
     assert word_decript == "ronildoomelhordetodos"
